02a311ba7738fa7c96e84aa77d7516cf78f6b8aa
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-04-03 15:08:26 +0300

    Blocker game show others

--- web/blocker-game/drawGame.js
+++ web/blocker-game/drawGame.js
@@ -2,95 +2,95 @@
  * Created by ekoodi on 28.3.2017.
  */
 gameApp.drawGame = function () {
-	
-	var canvas = document.getElementById("gameCanvas");
-	var ctx = canvas.getContext("2d");
 
-	var roadImg=new Image();
-	var mainImg = new Image();
-	var playerImg=new Image();
-	var coinImg= new Image();	
+    var canvas = document.getElementById("gameCanvas");
+    var ctx = canvas.getContext("2d");
 
-    var gameOn = true;	
+    var roadImg = new Image();
+    var mainImg = new Image();
+    var playerImg = new Image();
+    var coinImg = new Image();
+
+    var gameOn = true;
     var newWalltimer = 0;
-	var drawTimer;
+    var drawTimer;
     var timer;
-	var pointsTime = 50;
-	var points=0;
+    var pointsTime = 50;
+    var points = 0;
 
     var evilWalls = [];
-    var evilSpeed = 2;	
+    var evilSpeed = 2;
     var safeHight = 80;
 
-	var roadX = -10;
-	var coin;
-	
+    var roadX = -10;
+    var coin;
+
 
     function startGame() {
         player = gameApp.player(30, canvas.height / 2 - 25, 100, 50, "violet");
-		coin = gameApp.player(-100, 0, 40, 40, "yellow");
-		coinImg.src = "coin.png";
-		playerImg.src = "car.png";
-		roadImg.src = "road.png";
-		points=0;
-		timer = setInterval(gameClock, 10); 
-		drawTimer=setInterval(draw, 10);	 
+        coin = gameApp.player(-100, 0, 40, 40, "yellow");
+        coinImg.src = "coin.png";
+        playerImg.src = "car.png";
+        roadImg.src = "road.png";
+        points = 0;
+        timer = setInterval(gameClock, 10);
+        drawTimer = setInterval(draw, 10);
     }
-	
-	function endGame(){
-		    ctx.font = "100px Comic Sans MS";
-            ctx.fillStyle = "green";
-            ctx.textAlign = "center";
-            ctx.fillText("Game Over", canvas.width/2, canvas.height/2);
-			youCanStartPlay=true;
-	}
+
+    function endGame() {
+        ctx.font = "100px Comic Sans MS";
+        ctx.fillStyle = "green";
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
+        youCanStartPlay = true;
+    }
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-	    roadUpdate();
+        roadUpdate();
         evilUpdate();
-		coinUpdate();
+        coinUpdate();
         playerUpdate();
-		pointsUpdate();
+        pointsUpdate();
 
-		if (!gameOn) {
-           // requestAnimationFrame(draw); // one way make a loop
-			clearInterval(drawTimer);
-		    clearInterval(timer);
-			endGame();
+        if (!gameOn) {
+            // requestAnimationFrame(draw); // one way make a loop
+            clearInterval(drawTimer);
+            clearInterval(timer);
+            endGame();
         }
     }
-	
-	function pointsUpdate(){	
-		ctx.font= "20px Comic Sans MS";
-		ctx.fillStyle = "white";
-		ctx.textAlign = "left";
-		ctx.fillText("Points: "+points, 130, 20);
-	}
+
+    function pointsUpdate() {
+        ctx.font = "20px Comic Sans MS";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "left";
+        ctx.fillText("Points: " + points, 130, 20);
+    }
 
     function gameClock() {
         if (newWalltimer < 0 && gameOn) {
             newWalltimer = Math.floor(Math.random() * 260) + 160;
             newEvilWall();
-			if(coin.x+coin.width+10<0)
-				newCoin();
+            if (coin.x + coin.width + 10 < 0)
+                newCoin();
         }
         newWalltimer--;
-		
-		if(pointsTime<=0 && gameOn){
-			pointsTime=50;
-			points++;
-		}
-		pointsTime--;
+
+        if (pointsTime <= 0 && gameOn) {
+            pointsTime = 50;
+            points++;
+        }
+        pointsTime--;
     }
 
-	function roadUpdate(){
-		roadX -= evilSpeed;
-		if(roadX < -400)
-			roadX = 0;
-		
-	    ctx.drawImage(roadImg, roadX, 0);
-	}
+    function roadUpdate() {
+        roadX -= evilSpeed;
+        if (roadX < -400)
+            roadX = 0;
+
+        ctx.drawImage(roadImg, roadX, 0);
+    }
 
     function evilUpdate() {
 
@@ -102,17 +102,17 @@ gameApp.drawGame = function () {
             // downer wall
             ctx.fillRect(evilWalls[i].x, evilWalls[i].downerY, evilWalls[i].width, evilWalls[i].downerHeight);
 
-			// hit the wall test
+            // hit the wall test
             var safeY1 = evilWalls[i].upperY + evilWalls[i].upperHeight;
             var safeY2 = evilWalls[i].downerY;
-			var bumber=3; // this or player container box
-			
-			var playerHitVertical = (evilWalls[i].x <= player.x + player.width-bumber) && evilWalls[i].x + evilWalls[i].width > player.x+bumber;
+            var bumber = 3; // this or player container box
+
+            var playerHitVertical = evilWalls[i].x <= player.x + player.width - bumber && evilWalls[i].x + evilWalls[i].width > player.x + bumber;
             if (playerHitVertical) {
-				
-				var playerHitHorizontal = player.y+bumber < safeY1 || player.y + player.height-bumber > safeY2;
+
+                var playerHitHorizontal = player.y + bumber < safeY1 || player.y + player.height - bumber > safeY2;
                 if (playerHitHorizontal) {
-					gameOn = false;
+                    gameOn = false;
                 }
             }
 
@@ -121,36 +121,36 @@ gameApp.drawGame = function () {
     } // evil update end
 
     function playerUpdate() {
-		ctx.drawImage(playerImg, player.x, player.y);
+        ctx.drawImage(playerImg, player.x, player.y);
     }
-	
-	function coinUpdate(){
-		coin.x -= evilSpeed;
-		ctx.drawImage(coinImg, coin.x, coin.y);
-		
-		var coinHorizontalHit=(coin.x <= player.x + player.width) && coin.x + coin.width > player.x;
-		if (coinHorizontalHit) {
 
-			var coinVerticalHit=player.y+player.height>=coin.y &&player.y<coin.y+coin.height;
-			if(coinVerticalHit){
-				points=points+100;
-				coin.x=-100;	
-			}
+    function coinUpdate() {
+        coin.x -= evilSpeed;
+        ctx.drawImage(coinImg, coin.x, coin.y);
+
+        var coinHorizontalHit = (coin.x <= player.x + player.width) && coin.x + coin.width > player.x;
+        if (coinHorizontalHit) {
+
+            var coinVerticalHit = player.y + player.height >= coin.y && player.y < coin.y + coin.height;
+            if (coinVerticalHit) {
+                points = points + 100;
+                coin.x = -100;
+            }
         }
-	}
-	
-	function newCoin(){
-		y = Math.floor(Math.random() * (canvas.height-120))+60;
-		console.log("coin y: " +y);
-		x = canvas.width + Math.floor((Math.random() * 120))+180;
-		coin.x=x;
-		coin.y=y;
-	}
+    }
+
+    function newCoin() {
+        y = Math.floor(Math.random() * (canvas.height - 120)) + 60;
+        console.log("coin y: " + y);
+        x = canvas.width + Math.floor((Math.random() * 120)) + 180;
+        coin.x = x;
+        coin.y = y;
+    }
 
 
     function newEvilWall() {
         var id = 0;
-		safeHight = Math.floor((Math.random() * 120))+ player.height + 10;
+        safeHight = Math.floor((Math.random() * 120)) + player.height + 10;
         var upperHeight = Math.floor((Math.random() * (canvas.height - safeHight )));
         var downerY = upperHeight + safeHight;
         var downerHeigth = canvas.height - downerY;
@@ -174,39 +174,38 @@ gameApp.drawGame = function () {
         else
             evilWalls[evil.id - 1] = evil;
     }
-	
 
 
-	// Wait for mainimage
-	function loadRoad (callback){
-		mainImg.onload=callback;
-		mainImg.src = "road.png";
-		}
+    // Wait for main image
+    function loadRoad(callback) {
+        mainImg.onload = callback;
+        mainImg.src = "road.png";
+    }
 
-	function drawMain(){
-		ctx.clearRect(0, 0, canvas.width, canvas.height);	
-		ctx.drawImage(mainImg, 0, 0);
+    function drawMain() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(mainImg, 0, 0);
 
-		ctx.font = "90px Comic Sans MS";
+        ctx.font = "90px Comic Sans MS";
         ctx.fillStyle = "black";
         ctx.textAlign = "center";
-        ctx.fillText("Blocker Game", canvas.width/2, 170);
-		
-		ctx.font = "60px Comic Sans MS";
+        ctx.fillText("Blocker Game", canvas.width / 2, 170);
+
+        ctx.font = "60px Comic Sans MS";
         ctx.fillStyle = "green";
-        ctx.fillText("Push Start", canvas.width/2, 280);
-	}
+        ctx.fillText("Push Start", canvas.width / 2, 280);
+    }
 
 
     return {
         startGame: function () {
             startGame();
         },
-		drawDefault: function(){
-			loadRoad(drawMain);
-			
-		}
+        drawDefault: function () {
+            loadRoad(drawMain);
+
+        }
 
     }
-	
+
 }
